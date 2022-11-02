98d1ef7d2eae5af31f318011ef9f165fd3579f7c
apsalti <114912352+apsalti@users.noreply.github.com>
2022-11-02 15:56:46 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -15,10 +15,24 @@ answer = 0
 for i in range(np.size(P,0)):
     P_perturbe = f.perturb_pattern(P[i], 200)
     P_iter = f.dynamics(P_perturbe, f.hebbian_weights(P), 20)
-    if (P_iter != P[i]).all():
+    if (P_iter[-1] != P[i]).all():
         answer += 1
 
 if answer != 0:
     print('the first test failed, there are', answer, 'differences')
 elif answer == 0:
     print('the first test passed')
+
+patterns = f.generate_patterns(80,1000) 
+answer_2 = 0 
+
+for i in range(np.size(patterns,0)):
+    perturbed_pattern = f.perturb_pattern(patterns[i], 200)
+    historic_pertubed_p = f.dynamics_async(perturbed_pattern, f.hebbian_weights(patterns), 10000,3000)
+    if (historic_pertubed_p[-1] != patterns[i]).all():
+        answer_2 += 1
+
+if answer_2 != 0:
+    print('the second test failed, there are', answer_2, 'differences')
+else :
+    print('the second test passed')
